77b7ebb851e48c12ca2bf642e419341a9b1cc23e
Jesper Persson <Jesper.Persson@navico.com>
2021-04-20 12:54:02 +0200

    Move server example to its own project folder

--- example-acl-with-server/src/lib.rs
+++ example-acl-with-server/src/lib.rs
@@ -1,12 +1,23 @@
 // Has to be included, to get the errors and success parameters that are used in the
 // generate_dynamic_library macro invocation
 use mosquitto_plugin::*;
+use serde::Deserialize;
+use simple_server::{Method, Server, StatusCode};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
 // Some simple nonsense structure to use as an example
 #[derive(Debug)]
 pub struct Test {
     i: i32,
     s: String,
+    users: Arc<Mutex<HashMap<String, String>>>,
+}
+
+#[derive(Debug, Deserialize)]
+struct User {
+    user: String,
+    password: String,
 }
 
 // Required trait implementation
@@ -19,9 +30,55 @@ impl MosquittoPlugin for Test {
         let level = opts.get("level").unwrap_or(&default);
         let level = level.parse().unwrap_or(0);
 
+        let host = "127.0.0.1";
+        let port = "7878";
+
+        let users = HashMap::new();
+        let users = Arc::new(Mutex::new(users));
+
+        let users_clone = Arc::clone(&users);
+        let server = Server::new(move |request, mut response| {
+            println!("Request received. {} {}", request.method(), request.uri());
+
+            match (request.method(), request.uri().path()) {
+                (&Method::GET, "/hello") => Ok(response
+                    .body("<h1>Hi!</h1><p>Hello Rust!</p>".as_bytes().to_vec())
+                    .expect("Failed to write hello")),
+                (&Method::POST, "/new-user") => {
+                    Ok(match serde_json::from_slice::<User>(request.body()) {
+                        Ok(user) => {
+                            let mut users_clone = users_clone.lock().unwrap();
+                            users_clone.insert(user.user, user.password);
+                            response
+                                .body("<h1>Hi!</h1><p>Created new user!</p>".as_bytes().to_vec())
+                                .expect("Failed to write new user response")
+                        }
+                        Err(e) => response.body(
+                            format!(
+                                "<h1>Hi!</h1><p> Failed to create new user with error: {}</p>",
+                                e
+                            )
+                            .as_bytes()
+                            .to_vec(),
+                        )?,
+                    })
+                }
+                (_, _) => {
+                    response.status(StatusCode::NOT_FOUND);
+                    Ok(response
+                        .body("<h1>404</h1><p>Not found!<p>".as_bytes().to_vec())
+                        .expect("Failed to write 'not found' response"))
+                }
+            }
+        });
+        std::thread::spawn(move || {
+            server.listen(host, port);
+        });
+
         Test {
             i: level,
             s: topic.to_string(),
+            users,
         }
     }
     fn username_password(
@@ -37,9 +94,16 @@ impl MosquittoPlugin for Test {
         let u = u.unwrap();
         let p = p.unwrap();
 
-        // this will allow all username/password where the password is the username in reverse
-        let rp: String = p.chars().rev().collect();
-        if rp == u {
+        let user_in_users = {
+            let users = self.users.lock().unwrap();
+            if users.contains_key(u) && users.get(u).unwrap() == p {
+                true
+            } else {
+                false
+            }
+        };
+        println!("user_in_users {}", user_in_users);
+        if user_in_users {
             // Declare the accepted new client
             self.broker_broadcast_publish(
                 "new_client",
